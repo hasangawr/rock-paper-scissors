8c9a7ced548f10ac6f5961ae7e1c4ae0996ff8f7
hasangawr <hasangawr@gmail.com>
2023-06-04 05:24:56 +0530

    Implement playRound function

--- game.js
+++ game.js
@@ -6,4 +6,27 @@ function getComputerChoice() {
     return selections[computerChoice]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    let result = ''
+
+    if (playerSelection === computerSelection) {
+        result = 'Your strategies align perfectly. It\'s a deadlock, a draw.'
+    } else {
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
+            result = 'Paper smothers your mighty rock. You lose!'
+        } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+            result = 'You rock! You win!'
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            result = 'The power of paper prevails. You win!'
+        } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+            result = 'Your paper is no match. You\'ve been outplayed. You lose!'
+        } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+            result = 'Your opponent\'s rock smashes your scissor. You lose!'
+        } else {
+            result = 'Your scissor slashes through the competition. You win!'
+        }
+    }
+
+    return result
+}
